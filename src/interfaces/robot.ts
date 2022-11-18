7f9646ace238de6391b624c2ed3776ccd6817bc3
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-11-18 23:03:18 +0100

    Added robot interface

---
+++ src/interfaces/robot.ts
@@ -0,0 +1,14 @@
+export type ProtoRobot = {
+    name?: string;
+    resistance?: string;
+    speed?: string;
+    creationDate?: string;
+};
+
+export type Robot = {
+    id: string;
+    name: string;
+    resistance: string;
+    speed: string;
+    creationDate: string;
+};
