1c954655574f2fe76640d46e12f6940fc0c11eb2
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-11-20 15:01:07 +0100

    Fixing Sonar

--- src/db/dbconnect.ts
+++ src/db/dbconnect.ts
@@ -6,5 +6,6 @@ export function dbConnect() {
         process.env.NODE_ENV !== 'test' ? 'RobotsDB' : 'RobotsDBTesting';
     const uri = `mongodb+srv://${USER}:${PASSWD}@${CLUSTER}/${DBName}?retryWrites=true&w=majority`;
     console.log(uri);
+    console.log('URI: ' + uri);
     return mongoose.connect(uri);
 }
