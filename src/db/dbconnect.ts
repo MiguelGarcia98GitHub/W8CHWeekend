bd0bac82d0af7c6a99dded3e8611ee8485577a5c
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-11-20 15:14:28 +0100

    Fixing SonarCloud

--- src/db/dbconnect.ts
+++ src/db/dbconnect.ts
@@ -5,6 +5,7 @@ export function dbConnect() {
     const DBName =
         process.env.NODE_ENV !== 'test' ? 'RobotsDB' : 'RobotsDBTesting';
     const uri = `mongodb+srv://${USER}:${PASSWD}@${CLUSTER}/${DBName}?retryWrites=true&w=majority`;
+    console.log('URI');
     console.log(uri);
     return mongoose.connect(uri);
 }
